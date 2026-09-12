93e6094cc5810281c474995fd5f6b4ba142bc5d7
BugeStudioTeam <creepynightmares@proton.me>
2026-09-12 09:47:58 +0800

    disabled M8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -60,6 +60,10 @@ android {
     buildFeatures {
         compose = true
         buildConfig = true
+    }
+
+    androidResources {
+        noCompress += listOf("m8")
     }
 
     packaging {
